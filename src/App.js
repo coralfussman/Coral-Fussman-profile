714e8f2826ebd79f597ee969ac2f5aa8b3b0c20e
coralfussman <coralfussman@gmail.com>
2020-08-06 22:14:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +8,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First commit to this project
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/coralfussman"
+          
         >
-          Learn React
+          Github
         </a>
+
       </header>
     </div>
   );
