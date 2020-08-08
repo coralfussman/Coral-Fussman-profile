ad000464b65b3b2fbd58356e7a2edd256a6d2fe6
coralfussman <coralfussman@gmail.com>
2020-08-07 22:10:07 -0400

    added svg curve

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Ripple from '../Ripple.svg';
 import '../App.scss';
 
 export default function Home() {
     return (
-        <div>
-            <h2>Just trying to create a ripple</h2>
-
-            <div className="request-loader">
-                <span> My Waves </span>
+        <div className="main">
+            <img src={Ripple} alt="ripple" className="ripple" height="50px" /> 
+            <div >
+            <br/>
+            <br/>
+                <div className="request-loader">
+                    <span> MY WAVES </span>
+                </div>
             </div>
         </div>
     )
