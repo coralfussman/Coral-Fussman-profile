5239a9cebffa29c093f996d2c94ba65ada315ab8
coralfussman <coralfussman@gmail.com>
2020-08-07 20:50:07 -0400

    Updates

---
+++ precache-manifest.014d5940803c51090bed808d58274fa2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfe409829d024ea9d8c1ad6a6f4e6b8",
+    "url": "/Coral-Fussman-profile/index.html"
+  },
+  {
+    "revision": "6eaeba9c9079759ec3b5",
+    "url": "/Coral-Fussman-profile/static/css/main.2e6d96d4.chunk.css"
+  },
+  {
+    "revision": "f5307c154f27ab4ef50f",
+    "url": "/Coral-Fussman-profile/static/js/2.e835e31a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Coral-Fussman-profile/static/js/2.e835e31a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eaeba9c9079759ec3b5",
+    "url": "/Coral-Fussman-profile/static/js/main.b6be27df.chunk.js"
+  },
+  {
+    "revision": "104ea37ba1869d1799a5",
+    "url": "/Coral-Fussman-profile/static/js/runtime-main.594ee0d5.js"
+  },
+  {
+    "revision": "129e2f1f549d616a3b9de42210e8a16e",
+    "url": "/Coral-Fussman-profile/static/media/Github.129e2f1f.svg"
+  },
+  {
+    "revision": "3357b800222331b1e65c45a517d011b9",
+    "url": "/Coral-Fussman-profile/static/media/Linkedin.3357b800.svg"
+  },
+  {
+    "revision": "6d34e740adbdb3787a3cbb331ed81c68",
+    "url": "/Coral-Fussman-profile/static/media/Medium.6d34e740.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Coral-Fussman-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
