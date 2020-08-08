1fd96622f7145ab2e18a6aba54ea1833a0ac9c0f
coralfussman <coralfussman@gmail.com>
2020-08-08 15:06:48 -0400

    fixed minor browser bug render discrepencies

--- src/Components/ContactAlternate.js
+++ src/Components/ContactAlternate.js
@@ -16,19 +16,39 @@ function Contact() {
 
     const [submitted, setSubmitted] = useState(false)
    
+
+
+    const submitForm = (ev) => {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState !== XMLHttpRequest.DONE) return;
+          if (xhr.status === 200) {
+            form.reset();
+            this.setState({ status: "SUCCESS" });
+          } else {
+            this.setState({ status: "ERROR" });
+          }
+        };
+        xhr.send(data);
+      }
        
-    function sendEmail(e) {
-        e.preventDefault();
+    // function sendEmail(e) {
+    //     e.preventDefault();
     
-        emailjs.sendForm('gmail', 'my_email_service', e.target, 'user_xx0QNILgi7vUGs2yjhc5C')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
+    //     emailjs.sendForm('gmail', 'my_email_service', e.target, 'user_xx0QNILgi7vUGs2yjhc5C')
+    //       .then((result) => {
+    //           console.log(result.text);
+    //       }, (error) => {
+    //           console.log(error.text);
+    //       });
 
          
-      }
+    //   }
 
       
       const toggle = () => {
@@ -38,17 +58,17 @@ function Contact() {
       if(submitted === (false)){
         return (
             <div>
-                <form onSubmit={sendEmail}>
+                <form action="https://formspree.io/xaypkyyq" method="POST"  onSubmit={submitForm}>
                 <ContactMe> Like what you see? <br/> Contact Me </ContactMe>
-                <EmailAdd> ** note form disabled on github pages please use form on site </EmailAdd>
+
                     <Label htmlFor="label">Name:</Label>
-                    <Input type="text" id="label" name="user_name" />
+                    <Input type="text" id="label" name="_name" />
                     
                     <Label htmlFor="label">Email:</Label>
-                    <Input type="email" id="label" name="user_email" />
+                    <Input type="email" id="label" name="_replyto" />
 
                     <Label htmlFor="label">Message:</Label>
-                    <InputMessage type="text" id="label" name="message" />
+                    <InputMessage type="text" id="label" name="_body" />
 
                     <InputSubmit id="label" type="submit" value="Send" onClick={toggle}/>
                     <EmailAdd> ↪  or email me at coralfussman@gmail.com, looking forward to the possibilities  ↩ </EmailAdd>
